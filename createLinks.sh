4dc9ac115c78e4097603a9163cd500b886910bcc
George Alegre <facu.alegre@gmail.com>
2020-06-18 12:46:00 -0300

    Adding p10k config.

--- createLinks.sh
+++ createLinks.sh
@@ -7,4 +7,5 @@ ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/myclirc ~/.myclirc
+ln -s ~/.dotfiles/p10k.zsh ~/.p10k.zsh
 ln -s ~/.dotfiles/git_template ~/.git_template
